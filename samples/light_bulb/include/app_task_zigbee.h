4ffa855a75e815b1ff57c138a9dcfd6b38996e98
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2026-03-26 11:08:02 +0100

    samples: combined Matter + Zigbee Application rework
    - Created two separate app tasks for Matter and Zigbee.
    - Combined two protocols in the main.cpp file.
    - Rework of configuration to be compliant with Matter.
    - Added a variant to sample.yaml.
    - Fixed nrf54lm20 configuration.
    
    Signed-off-by: Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>

---
+++ samples/light_bulb/include/app_task_zigbee.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2026 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef APP_TASK_ZIGBEE_H
+#define APP_TASK_ZIGBEE_H 1
+
+void ZigbeeStart(void);
+
+#endif /* APP_TASK_ZIGBEE_H */
