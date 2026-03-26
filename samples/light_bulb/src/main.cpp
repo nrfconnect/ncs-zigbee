a50cca03938190870aaba2992e6172cd3ab60614
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2026-03-26 11:08:02 +0100

    Combained Matter + Zigbee Application rework
    - Created two separate app tasks for Matter and Zigbee.
    - Combained two protocols in the main.cpp file.
    - Rework of configuration to be compliant with Matter.
    - Added a variant to sample.yaml.
    - Fixed nrf54lm20 configuration.
    
    Signed-off-by: Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>

---
+++ samples/light_bulb/src/main.cpp
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2024-2026 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/** @file
+ *
+ * @brief Simple Zigbee light bulb implementation.
+ */
+
+#include "app_task_zigbee.h"
+
+#ifdef CONFIG_CHIP
+
+#include "app_task_matter.h"
+#include <app/matter_event_handler.h>
+#include <nrf_802154_callbacks_dispatcher.h>
+#include <openthread.h>
+
+extern "C" {
+#include <zb_nrf_platform.h>
+}
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(app, CONFIG_CHIP_APP_LOG_LEVEL);
+
+/* Zephyr thread stack size and priority */
+#define ZIGBEE_THREAD_STACK_SIZE 2048
+#define ZIGBEE_THREAD_PRIORITY 3
+#define MATTER_THREAD_STACK_SIZE 3072
+#define MATTER_THREAD_PRIORITY 3
+
+namespace {
+
+void zigbee_thread_fn() {
+  int ret = nrf_802154_callbacks_dispatcher_switch("zigbee");
+  if (ret != 0) {
+    LOG_ERR("Failed to switch 802.15.4 radio to Zigbee: %d", ret);
+  }
+
+  ZigbeeStart();
+}
+void matter_thread_fn() { AppTask::Instance().StartApp(); }
+
+K_THREAD_DEFINE(zigbee_thread_id, ZIGBEE_THREAD_STACK_SIZE, zigbee_thread_fn,
+                NULL, NULL, NULL, ZIGBEE_THREAD_PRIORITY, 0, 0);
+
+K_THREAD_DEFINE(matter_thread_id, MATTER_THREAD_STACK_SIZE, matter_thread_fn,
+                NULL, NULL, NULL, MATTER_THREAD_PRIORITY, 0, 0);
+
+void switch_to_thread_radio(void) {
+  k_thread_abort(zigbee_thread_id);
+  zigbee_deinit();
+  int ret = nrf_802154_callbacks_dispatcher_switch("openthread");
+  if (ret != 0) {
+    LOG_ERR("Failed to switch 802.15.4 radio to Thread: %d", ret);
+  }
+}
+
+void matter_zigbee_event_handler(
+    const chip::DeviceLayer::ChipDeviceEvent *event, intptr_t arg) {
+  switch (event->Type) {
+  case chip::DeviceLayer::DeviceEventType::kSecureSessionEstablished:
+    /* Switch to Thread Radio and disable Zigbee Stack*/
+    if (!chip::DeviceLayer::ThreadStackMgr().IsThreadAttached()) {
+      switch_to_thread_radio();
+    }
+    break;
+  case chip::DeviceLayer::DeviceEventType::kFactoryReset:
+    /* Remove also the zigbee stack data */
+    zigbee_pibcache_pan_id_clear();
+  default:
+    break;
+  }
+}
+} // namespace
+
+#endif /* CONFIG_CHIP */
+
+int main(void) {
+
+#ifdef CONFIG_CHIP
+
+  Nrf::Matter::RegisterEventHandler(matter_zigbee_event_handler, 0);
+
+  k_thread_start(zigbee_thread_id);
+  k_thread_start(matter_thread_id);
+
+#else
+
+  ZigbeeStart();
+
+#endif
+
+  return 0;
+}
